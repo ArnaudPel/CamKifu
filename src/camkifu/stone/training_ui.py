5d3dbc84fe922de5679b624ddb753bfce2faf464
Arnaud <arnaud.peloquin@gmail.com>
2016-09-09 16:05:56 +0200

    UI to verify sampling is now usable

--- src/camkifu/stone/training_ui.py
+++ src/camkifu/stone/training_ui.py
@@ -1,5 +1,5 @@
 import tkinter
-from os import listdir
+from os import listdir, remove
 from tkinter.ttk import Frame, Label, Button
 
 from os.path import join, isfile
@@ -9,11 +9,48 @@ import numpy as np
 from camkifu.stone.sf_neural import TRAIN_DAT_SUFFIX, SfNeural
 from glmain import configure, place, bring_to_front
 
-DELETE = "Delete"
 
 PNG_SUFFIX = ".png"
-SAMPLED = "Sampled"
+
+VERIFY = "Verify"
+DELETE = "Delete"
+
 NEW = "New"
+SAMPLED = "Sampled"
+
+
+class Row:
+    def __init__(self, nnframe, idx, path, button):
+        self.nnframe = nnframe
+        self.idx = idx
+        self.path = path
+        self.status = tkinter.StringVar(master=nnframe)
+        self.button = button
+        if isfile(self.path.replace(PNG_SUFFIX, TRAIN_DAT_SUFFIX)):
+            self.mark_as_done()
+        else:
+            self.mark_as_new()
+
+    def sample(self):
+        self.nnframe.gen_data(self.path)
+        self.mark_as_done()
+
+    def mark_as_done(self):
+        self.status.set(SAMPLED)
+        self.button["text"] = DELETE
+        self.button["command"] = self.delete
+
+    def delete(self):
+        try:
+            remove(self.path.replace(PNG_SUFFIX, TRAIN_DAT_SUFFIX))
+        except Exception as e:
+            print(e)
+        self.mark_as_new()
+
+    def mark_as_new(self):
+        self.status.set(NEW)
+        self.button["text"] = VERIFY
+        self.button["command"] = self.sample
 
 
 class NnFrame(Frame):
@@ -21,36 +58,21 @@ class NnFrame(Frame):
         Frame.__init__(self, master)
         self.dir = train_dir
         self.sf = SfNeural(None)
-        self.blist = []
+        self.rows = []
 
         for i, img in enumerate(f for f in listdir(self.dir) if f.endswith(PNG_SUFFIX)):
-            status = tkinter.StringVar(master=self)
-            if isfile(join(self.dir, img.replace(PNG_SUFFIX, TRAIN_DAT_SUFFIX))):
-                status.set(SAMPLED)
-                btext = DELETE
-
-                def bcommand(idx=i):
-                    print("Todo - butt" + str(idx))
-            else:
-                status.set(NEW)
-                btext = "Verify"
-
-                def bcommand(n=img, s=status, idx=i):
-                    self.gen_data(n, s, idx)
-            button = Button(self, text=btext, command=bcommand)
+            button = Button(self)
             button.grid(row=i, column=2)
-            self.blist.append(button)
-
+            row = Row(self, i, join(self.dir, img), button)
+            self.rows.append(row)
             Label(self, text=img).grid(row=i)
-            Label(self, textvariable=status).grid(row=i, column=1)
+            Label(self, textvariable=row.status).grid(row=i, column=1)
 
-    def gen_data(self, img_name, svar, idx):
+    def gen_data(self, img_name):
         fpath = join(self.dir, img_name)
         mpath = fpath.replace(PNG_SUFFIX, TRAIN_DAT_SUFFIX)
         x, y = self.sf.gen_data(fpath)
         np.savez(mpath, X=x, Y=y)
-        svar.set(SAMPLED)
-        self.blist[idx]["text"] = DELETE
 
 
 if __name__ == '__main__':
